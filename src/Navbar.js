c0d3a858db14b72f3dac83cd6febb27a058866aa
Domeshow <domeshowemmanuel@gmail.com>
2023-01-04 18:07:14 +0200

    Change project title

--- src/Navbar.js
+++ src/Navbar.js
@@ -2,7 +2,7 @@ const Navbar = () => {
     
     return ( 
         <nav className="navbar">
-            <h1>The Dojo Blog</h1>
+            <h1>TheReal</h1>
             <div className="links">
                 <a href="/">Home</a>
                 <a href="/create">New Blog</a>
