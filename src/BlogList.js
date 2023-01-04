b5a7f5d01bcc25d8f8365d94a4d9e2b2cbaa1513
Domeshow <domeshowemmanuel@gmail.com>
2023-01-04 18:17:00 +0200

    Count blogs

--- src/BlogList.js
+++ src/BlogList.js
@@ -2,7 +2,7 @@ const BlogList = ({blogs, title, handleDelete}) => {
     
     return ( 
         <div className="blog-list">
-            <h3>{title}</h3>
+            <h3>{title} ({blogs.length})</h3>
             {blogs.map((blog) => (
                 <div className="blog-preview" key={blog.id}>
                     <h2>{blog.title}</h2>
