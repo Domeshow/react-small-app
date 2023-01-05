1c8bdae141c46da07785162257806d7f461f19f6
Domeshow <domeshowemmanuel@gmail.com>
2023-01-05 14:06:51 +0200

    Fetching data with useEffect

--- src/Home.js
+++ src/Home.js
@@ -1,19 +1,25 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        { id: 1, title: "Blog 1 Title", body: "Lorem Ipsum is simply dummy text of the printing and typesetting industry.", author: "Domeshow" },
-        { id: 2, title: "Blog 2 Title", body: "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.", author: "Jonathan" },
-    ]);
+    const [blogs, setBlogs] = useState(null);
     
     const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id);console.log(newBlogs)
         setBlogs(newBlogs);
     }
+
+    useEffect(() => {
+        fetch("http://localhost:8000/blogs")
+        .then(res=> {
+            return res.json()
+        }).then(data => {
+            setBlogs(data)
+        })
+    }, []);
     return (
         <div className="home">
-            <BlogList blogs={blogs} title="All blogs" handleDelete={handleDelete}/>
-            <BlogList blogs={blogs.filter((blog)=> blog.author === "Domeshow")} title="Domeshow's blogs" handleDelete={handleDelete}/>
+            { blogs && <BlogList blogs={blogs} title="All blogs" handleDelete={handleDelete}/> }
+            { blogs && <BlogList blogs={blogs.filter((blog)=> blog.author === "Domeshow")} title="Domeshow's blogs" handleDelete={handleDelete}/> }
         </div>
     );
 }
