0a1cbe7fc60a66960bdeaec5046e0b434f5406e0
Domeshow <domeshowemmanuel@gmail.com>
2023-01-05 14:12:33 +0200

    Conditional Loading Message

--- src/Home.js
+++ src/Home.js
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
+    const [isPending, setIsPending] = useState(true);
     
     const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id);console.log(newBlogs)
@@ -9,15 +10,19 @@ const Home = () => {
     }
 
     useEffect(() => {
-        fetch("http://localhost:8000/blogs")
-        .then(res=> {
-            return res.json()
-        }).then(data => {
-            setBlogs(data)
-        })
+        setTimeout(()=> {
+            fetch("http://localhost:8000/blogs")
+            .then(res=> {
+                return res.json()
+            }).then(data => {
+                setBlogs(data)
+                setIsPending(false);
+            })
+        }, 1000);
     }, []);
     return (
         <div className="home">
+            { isPending && <div>Loading...</div> }
             { blogs && <BlogList blogs={blogs} title="All blogs" handleDelete={handleDelete}/> }
             { blogs && <BlogList blogs={blogs.filter((blog)=> blog.author === "Domeshow")} title="Domeshow's blogs" handleDelete={handleDelete}/> }
         </div>
