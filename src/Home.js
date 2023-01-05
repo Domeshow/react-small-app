3b2af83a5b574e297e274e3633c4c67f0eccc225
Domeshow <domeshowemmanuel@gmail.com>
2023-01-05 14:37:50 +0200

    Handling Fetch Errors

--- src/Home.js
+++ src/Home.js
@@ -3,6 +3,7 @@ import BlogList from "./BlogList";
 const Home = () => {
     const [blogs, setBlogs] = useState(null);
     const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
     
     const handleDelete = (id) => {
         const newBlogs = blogs.filter(blog => blog.id !== id);console.log(newBlogs)
@@ -13,9 +14,16 @@ const Home = () => {
         setTimeout(()=> {
             fetch("http://localhost:8000/blogs")
             .then(res=> {
-                return res.json()
+                if (!res.ok)
+                    throw Error("Could not fetch the data")
+                return res.json(); //Else
             }).then(data => {
                 setBlogs(data)
+                setIsPending(false);
+                setError(null);
+            }).catch(err => {
+                console.log(err.message)
+                setError(err.message)
                 setIsPending(false);
             })
         }, 1000);
@@ -23,6 +31,7 @@ const Home = () => {
     return (
         <div className="home">
             { isPending && <div>Loading...</div> }
+            { error && <div> {error} </div> }
             { blogs && <BlogList blogs={blogs} title="All blogs" handleDelete={handleDelete}/> }
             { blogs && <BlogList blogs={blogs.filter((blog)=> blog.author === "Domeshow")} title="Domeshow's blogs" handleDelete={handleDelete}/> }
         </div>
